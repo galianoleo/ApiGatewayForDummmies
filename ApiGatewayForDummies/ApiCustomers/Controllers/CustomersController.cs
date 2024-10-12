f47aa06992e7b9627971accc56a236b30a8bb03c
lgaliano@mvfactory.com <lgaliano@mvfactory.com>
2024-10-12 12:49:03 -0300

    upload api gateway for dummies

---
+++ ApiGatewayForDummies/ApiCustomers/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCustomers.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new Customer { Id=1, Name = "Nombre del cliente"});
+        }
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ApiGatewayForDummies/ApiUsers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiUsers
+{
+    [Produces("application/json")]
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new User { Id = 1, Nombre = "Nombre de usuario" });
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
